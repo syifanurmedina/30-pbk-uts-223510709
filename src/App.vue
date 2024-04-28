5a6ae742f1de3532b315df0fd1c732901a58ac37
syifanurmedina <irishollyhock0@gmail.com>
2024-04-28 23:09:15 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+let id = 0
+
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+  { id: id++, text: 'Leton coffee', done: true },
+  { id: id++, text: 'Tomoro coffee', done: true },
+  { id: id++, text: 'Kaua coffee', done: true },
+  { id: id++, text: 'Soe coffee', done: false },
+  { id: id++, text: 'Fore coffee', done: false }
+])
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value
+    ? todos.value.filter((t) => !t.done)
+    : todos.value
+})
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+</script>
+
+<template>
+  <div id="app">
+    <img alt="Vue Logo" src="./assets/coffe2.jpeg" height="200">
+    <logos />
+  </div>
+  <h2>COFFE SHOP YANG DI KUNJUNGI</h2>
+  <h3>By: Syifa Nur Medina</h3>
+
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="isi disini">
+    <button>Tambah +</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Tampilkan' : 'Sembunyikan' }}
+  </button>
+</template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<!-- src/components/ToDoList.vue -->
+<template>
+  <div>
+    <h2>To-Do List</h2>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Tambah tugas...">
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+        <button @click="deleteTodo(index)">Hapus</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push(this.newTodo);
+        this.newTodo = '';
+      }
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Styling komponen ToDoList */
+</style>
